e79f4069640a5ee3cb65aa6f67dc86e5d1432fbc
ThatGuy123-cmd <55110201+ThatGuy123-cmd@users.noreply.github.com>
2022-03-10 15:31:33 +0000

    classes updated

--- CardsGoingPostal/payment.php
+++ CardsGoingPostal/payment.php
@@ -1,17 +1,1 @@
 <?php declare(strict_types=1);
-
-class Payments{
-
-    public $nameOnCard;
-    public $nnNr;
-    public $expDate;
-    public $secCode;
-
-    function charge($amount)
-    {
-
-
-    }
-
-
-}
--- CardsGoingPostal/printer.php
+++ CardsGoingPostal/printer.php
@@ -1,10 +1,1 @@
 <?php declare(strict_types=1);
-
-class Printer{
-    public $empNr;
-
-
-    function changeShipmentStatus(){
-
-    }
-}
