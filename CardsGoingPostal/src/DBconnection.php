5575f4bcb4af68472c661d61c868542b6afa76b4
Time2Rage <renedralle@yahoo.de>
2022-04-04 16:20:03 +0100

    Editor setup

--- CardsGoingPostal/src/DBconnection.php
+++ CardsGoingPostal/src/DBconnection.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../src/config.php";
+require "../src/config.php";
 
         try {
             $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $DBpassword, $options);
